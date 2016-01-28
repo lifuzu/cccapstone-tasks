b39bf7090530cb86e0db13b452944aaaf09b2b31
Fuzu Li <lifuzu@gmail.com>
2016-01-28 02:49:50 -0500

    Glue PIG with Cassandra for group2_2

---
+++ task1_part2_group2_2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+FILE=task1_part2_group2_2
+
+# CLEAN UP the environment
+rm -fr $FILE.csv*
+hadoop fs -rm -r '/cccapstone/output/'"$FILE"
+
+# RUN PIG script
+pig -f $FILE.pig -param output=$FILE
+
+# EXPORT to csv
+hadoop fs -getmerge '/cccapstone/output/'"$FILE" "$FILE".csv
+
+# FORMAT csv
+#tr -s ',' '\t' < $FILE.csv.raw | tr -d '()' > $FILE.csv
+
+# IMPORT to Cassandra
+cqlsh -f $FILE.cql
