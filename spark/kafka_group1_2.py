b4bc77c0d92e5be16854373064ed8630011212a5
Fuzu Li <lifuzu@gmail.com>
2016-02-16 19:02:13 -0500

    Complete Group 1.2

--- spark/kafka_group1_2.py
+++ spark/kafka_group1_2.py
@@ -8,6 +8,7 @@ appName = "group1-2"
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SQLContext, Row
 
 def exit_gracefully(ssc):
     print("Gracefully stopping Spark Streaming Application")
@@ -18,16 +19,43 @@ def exit_gracefully(ssc):
 def updateFunc(new_values, last_sum):
     return sum(new_values) + (last_sum or 0)
 
+def getSqlContextInstance(sparkContext):
+    if ('sqlContextSingletonInstance' not in globals()):
+        globals()['sqlContextSingletonInstance'] = SQLContext(sparkContext)
+    return globals()['sqlContextSingletonInstance']
+
 def main(ssc):
     zkQuorum, topic = sys.argv[1:]
-    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 2})
     lines = kvs.map(lambda x: x[1])
-    data = lines.map(lambda line: line.split(",")) \
-        .map(lambda word: (word[0], float(word[1])) ) \
-        .reduceByKey(lambda a, b: a+b) \
-        .updateStateByKey(updateFunc) \
-        .transform(lambda rdd: rdd.sortBy(lambda (word, count): -count))
-    data.pprint()
+
+    # Convert RDDs of the words DStream to DataFrame and run SQL query
+    def process(time, rdd):
+        print("========= %s =========" % str(time))
+        try:
+            # Get the singleton instance of SQLContext
+            sqlContext = getSqlContextInstance(rdd.context)
+            # Convert RDD[String] to RDD[Row] to DataFrame
+            parts = rdd.map(lambda line: line.split(","))
+            delays= parts.map(lambda w: Row(carrier=w[0], delay=float(w[1])))
+            dataFrame = sqlContext.createDataFrame(delays)
+            # Register as table
+            dataFrame.registerTempTable("carrier_delays")
+            # Do word count on table using SQL and print it
+            carrier_delays_df = \
+                sqlContext.sql("SELECT carrier, avg(delay) AS avg_delay FROM carrier_delays GROUP BY carrier ORDER BY avg_delay ASC LIMIT 10")
+            carrier_delays_df.show()
+        except Exception as e: print (e)
+        #except:
+        #    pass
+    #data = lines.map(lambda line: line.split(",")) \
+    #    .map(lambda word: (word[0], float(word[1])) ) \
+    #    .aggregateByKey((0,0), lambda a,b: (a[0] + b, a[1] + 1), lambda a,b: (a[0] + b[0], a[1] + b[1])) \
+    #    .mapValues(lambda v: v[0]/v[1]) \
+    #    .updateStateByKey(updateFunc) \
+    #    .transform(lambda rdd: rdd.sortBy(lambda (word, count): -count))
+    #data.pprint()
+    lines.foreachRDD(process)
 
     ssc.start()
     ssc.awaitTermination()
@@ -42,6 +70,7 @@ if __name__ == "__main__":
     #sc = SparkContext(appName="PythonStreamingKafkaWordCount")
     ssc = StreamingContext(sc, 10)
     ssc.checkpoint("checkpoint")
+    ssc.remember(60)
 
     main(ssc)
     #try:
